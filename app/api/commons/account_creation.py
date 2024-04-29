d9f176f4d95e02f6feaa9994bcb564369146a85a
nouman baig <91089058+noumannbaig@users.noreply.github.com>
2024-04-29 20:29:47 +0500

    headless mode enabled

--- app/api/commons/account_creation.py
+++ app/api/commons/account_creation.py
@@ -154,7 +154,7 @@ def create_gmail_account(
         user_agent = UserAgent(browsers=['chrome'])
         chrome_options = Options()
 
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
