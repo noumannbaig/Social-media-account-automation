525953495b8e4758034825da53da1870819d6354
nouman baig <91089058+noumannbaig@users.noreply.github.com>
2024-09-17 20:15:04 +0500

    Update engine.py

--- app/database/engine.py
+++ app/database/engine.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://avatar:avatar@37.27.25.194/avatardb"
+SQLALCHEMY_DATABASE_URL = "db_creds"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
